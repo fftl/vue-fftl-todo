22f1b8f726f3321a364fde276d0e325145d94781
Dongmin Lee <fftl0785@gmail.com>
2025-09-21 18:22:40 +0900

    feat : member insert check

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,11 +1,34 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import { LoginRequest } from '@/types/member'
+import { idCheck, signUp } from '@/services/member'
 
 const username = ref('')
 const password = ref('')
+const passwordCheck = ref('')
+
+function doIdCheck() {
+  idCheck(username.value)
+}
+
+function doSignUp() {
+  const loginRequest: LoginRequest = {
+    username: username.value,
+    password: password.value,
+  }
+
+  signUp(loginRequest)
+}
 </script>
 
 <template>
-  <input type="text" v-bind:value="username" />
-  <input type="password" v-bind:value="password" />
+  <h2>회원가입</h2>
+  <span>아이디 : </span><input type="text" v-model="username" /><button @click="doIdCheck">
+    아이디 중복확인
+  </button>
+  <div></div>
+  <span>비밀번호 : </span><input type="password" v-model="password" />
+  <div></div>
+  <span>비밀번호 확인: </span><input type="password" v-model="passwordCheck" />
+  <button @click="doSignUp">회원 가입</button>
 </template>
